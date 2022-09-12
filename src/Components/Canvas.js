569362522f74fcc3c0a0e372880adc4e40a73838
Mirzobakhtiyor Homidov <khamidov.mirzo@gmail.com>
2022-09-12 22:44:50 +0800

    implement page tagging

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -1,10 +1,61 @@
 import "../Assets/styles/Canvas.css";
 import Picker from "./Picker";
 import React, { useState } from "react";
+import artImage from "../Assets/img/art.jpeg";
 function Canvas() {
-  const [picker, setPicker] = useState(<Picker />);
+  const [coordinates, setCoordinates] = useState({ x: 0, y: 0 });
+  const [visibility, setVisibilty] = useState("");
+  // function switchPicker() {
+  //   console.log("click");
+  //   if (picker) {
+  //     setPicker(null);
+  //   } else {
+  //     setPicker(<Picker coordinates = {coordinates}/>);
+  //   }
+  // }
 
-  return <div className="canvas-container">{picker}</div>;
+  function onClickOnImage(event) {
+    if (visibility === "") {
+      setVisibilty("hide");
+    } else {
+      setVisibilty("");
+      // event.nativeEvent.offsetX/offsetY offset relative to clien's image size
+      const playArea = document.querySelector(".play-area");
+
+      // width and height of the play-area on client side
+      const clientHeight = playArea.clientHeight;
+      const clientWidth = playArea.clientWidth;
+
+      // offset by x&y on cliend side
+      const X = event.nativeEvent.offsetX;
+      const Y = event.nativeEvent.offsetY;
+
+      // calculate x&y in percents on client side
+      const xPercent = X / (clientWidth / 100);
+      const yPercent = Y / (clientHeight / 100);
+
+      console.log(`Coordinates: X= ${xPercent}%, Y = ${yPercent}%`);
+
+      const newCoordinates = {
+        x: xPercent,
+        y: yPercent,
+      };
+
+      setCoordinates(newCoordinates);
+    }
+  }
+
+  return (
+    <div className="canvas-container">
+      <div className="play-area" onClick={onClickOnImage}>
+        <Picker className={visibility} coordinates={coordinates} />
+        <img src={artImage} alt="" />
+        <div className="freeman"></div>
+        <div className="pikachu"></div>
+        <div className="little-big"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Canvas;
--- src/Components/Picker.js
+++ src/Components/Picker.js
@@ -1,12 +1,29 @@
-import '../Assets/styles/Picker.css';
+import "../Assets/styles/Picker.css";
 
-function Picker() {
+function Picker(props) {
+  console.log(props);
+  const PickerStyle = {
+    position: "absolute",
+    left: `${props.coordinates.x}%`,
+    top: `${props.coordinates.y}%`,
+  };
+
+  function checkFreeman() {
+    
+  }
+
   return (
-    <div className="picker-container">
-      <ul>
-        <li><button className="choice">Choice 1</button></li>
-        <li><button className="choice">Choice 2</button></li>
-        <li><button className="choice">Choice 3</button></li>
+    <div style={PickerStyle} className={`picker-container`}>
+      <ul className={props.className}>
+        <li>
+          <button className="choice">Choice 1</button>
+        </li>
+        <li>
+          <button className="choice">Choice 2</button>
+        </li>
+        <li>
+          <button className="choice">Choice 3</button>
+        </li>
       </ul>
     </div>
   );
